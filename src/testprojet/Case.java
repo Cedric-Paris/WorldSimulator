ea79263d16a3aac4a0255c3d15fd58d2eca38c5d
Kwayoo <ribierel@yahoo.fr>
2015-12-11 11:57:03 +0100

    Prémisse Case
    Case
    Monde
    non finit

---
+++ src/testprojet/Case.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testprojet;
+
+import java.util.List;
+import testprojet.Monde;
+import testprojet.Population;
+
+/**
+ *
+ * @author Ribière Laurent
+ */
+public class Case {
+    //Region Attribut/Propriete
+    private Monde monde;
+    public Monde getMonde() {return monde;}
+    public void setMonde(Monde value) {monde = value;}
+    
+    private Population population;
+    public Population getPopulation(){return population;}
+    public void setPopulation(Population value) {population = value;}
+        
+    private Terrain terrain;
+    public Terrain getTerrain() {return terrain;}
+    public void setTerrain(Terrain value) {terrain = value;}
+    //Fin de région
+    
+    //Region Methodes
+    
+    public List<Case> getVoisin() 
+    {
+        monde.findVoisinAtLoc();
+    }
+    
+    //Fin de Region
+    
+}
---
+++ src/testprojet/Monde.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testprojet;
+
+import java.util.List;
+
+/**
+ *
+ * @author Ribière Laurent
+ */
+public class Monde {
+    
+    private int damier[][];
+    private int longueurMax;
+
+    public Monde(int nbCase) {
+        this.longueurMax=(int)nbCase/2;
+        for(int i=0;i<nbCase-longueurMax;i+=longueurMax)
+        {
+            for(int j=0;j<longueurMax;j++)
+            {
+                //damier[j][]
+            }
+        }
+    }
+    
+    
+    //Region Methodes
+    public List<Case> FindTerrainAtLoc() 
+    {
+        
+    }
+}
